2d436e05082518fed861340a16e10b581f8fae2e
fsblaise <balazsolah19@gmail.com>
2022-04-20 19:34:22 +0200

    Added the new items, and collection.

---
+++ app/src/main/java/hu/fsblaise/kcal/IntakeActivity.java
@@ -0,0 +1,14 @@
+package hu.fsblaise.kcal;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class IntakeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intake);
+    }
+}
