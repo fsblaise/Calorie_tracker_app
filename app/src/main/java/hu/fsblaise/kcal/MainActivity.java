60982f4e247eb54c303702b5a70892d96cf3e877
fsblaise <balazsolah19@gmail.com>
2022-04-20 10:32:31 +0200

    Separated the login screen from the main activity. Now the main screen is hub, where the user can choose what they want to do.

--- app/src/main/java/hu/fsblaise/kcal/MainActivity.java
+++ app/src/main/java/hu/fsblaise/kcal/MainActivity.java
@@ -6,6 +6,7 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.loader.app.LoaderManager;
 import androidx.loader.content.Loader;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
@@ -27,131 +28,134 @@ import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.GoogleAuthProvider;
 
-public class MainActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<String> {
+public class MainActivity extends Activity {
     private static final String TAG = MainActivity.class.getName();
     private static final String PREF_KEY = MainActivity.class.getPackage().toString();
-    private static final int RC_SIGN_IN = 123;
-    private static final int SECRET_KEY = 99;
+    private static int SECRET_KEY;
+//    private static final int RC_SIGN_IN = 123;
+//    private static final int SECRET_KEY = 99;
 
-    EditText userNameET;
-    EditText passwordET;
-
-    private SharedPreferences preferences;
-    private FirebaseAuth mAuth;
-    private GoogleSignInClient mGoogleSignInClient;
+//    EditText userNameET;
+//    EditText passwordET;
+//
+//    private SharedPreferences preferences;
+//    private FirebaseAuth mAuth;
+//    private GoogleSignInClient mGoogleSignInClient;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        userNameET = findViewById(R.id.editTextUserName);
-        passwordET = findViewById(R.id.editTextPassword);
-
-        preferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
-        mAuth = FirebaseAuth.getInstance();
-
-        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
-                .requestIdToken(getString(R.string.default_web_client_id))
-                .requestEmail()
-                .build();
-        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
+        Bundle bundle = getIntent().getExtras();
+        SECRET_KEY = getIntent().getIntExtra("SECRET_KEY", 99);
+//        userNameET = findViewById(R.id.editTextUserName);
+//        passwordET = findViewById(R.id.editTextPassword);
+//
+//        preferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
+//        mAuth = FirebaseAuth.getInstance();
+//
+//        GoogleSignInOptions gso = new GoogleSignInOptions.Builder(GoogleSignInOptions.DEFAULT_SIGN_IN)
+//                .requestIdToken(getString(R.string.default_web_client_id))
+//                .requestEmail()
+//                .build();
+//        mGoogleSignInClient = GoogleSignIn.getClient(this, gso);
 
         // Random Async Task
 //        Button button = findViewById(R.id.guestLoginButton);
 //        new RandomAsyncTask(button).execute();
 
         // Random Async Loader
-        getSupportLoaderManager().restartLoader(0, null, this);
-        Log.i(TAG, "onCreate");
+//        getSupportLoaderManager().restartLoader(0, null, this);
+        Log.i(TAG, "onCreate" + "    " +SECRET_KEY);
     }
 
-    @Override
-    public void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
+//    @Override
+//    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (requestCode == RC_SIGN_IN) {
+//            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+//
+//            try {
+//                GoogleSignInAccount account = task.getResult(ApiException.class);
+//                Log.d(TAG, "firebaseAuthWithGoogle: " + account.getId());
+//                firebaseAuthWithGoogle(account.getIdToken());
+//            } catch (ApiException e) {
+//                Log.w(TAG, "Google sign in failed", e);
+//            }
+//        }
+//
+//    }
 
-        if (requestCode == RC_SIGN_IN) {
-            Task<GoogleSignInAccount> task = GoogleSignIn.getSignedInAccountFromIntent(data);
+//    private void firebaseAuthWithGoogle(String idToken) {
+//        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
+//        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//            @Override
+//            public void onComplete(@NonNull Task<AuthResult> task) {
+//                if (task.isSuccessful()) {
+//                    Log.d(TAG, "User logged in successfully!");
+//                    startShopping();
+//                } else {
+//                    Log.d(TAG, "Something went wrong...");
+//                    Toast.makeText(MainActivity.this, "User login fail: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+//                }
+//            }
+//        });
+//
+//    }
 
-            try {
-                GoogleSignInAccount account = task.getResult(ApiException.class);
-                Log.d(TAG, "firebaseAuthWithGoogle: " + account.getId());
-                firebaseAuthWithGoogle(account.getIdToken());
-            } catch (ApiException e) {
-                Log.w(TAG, "Google sign in failed", e);
-            }
-        }
+//    public void login(View view) {
+//        String userName = userNameET.getText().toString();
+//        String password = passwordET.getText().toString();
+//
+////        Log.i(TAG, "login: " + userName + "passwd: " + password);
+//
+//        mAuth.signInWithEmailAndPassword(userName, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//            @Override
+//            public void onComplete(@NonNull Task<AuthResult> task) {
+//                if (task.isSuccessful()) {
+//                    Log.d(TAG, "User logged in successfully!");
+//                    startShopping();
+//                } else {
+//                    Log.d(TAG, "Something went wrong...");
+//                    Toast.makeText(MainActivity.this, "User login fail: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+//                }
+//            }
+//        });
+//    }
 
-    }
+//    private void startShopping() {
+//        Intent intent = new Intent(this, ShopListActivity.class);
+//        startActivity(intent);
+//    }
+//
+//    public void loginWithGoogle(View view) {
+//        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
+//        startActivityForResult(signInIntent, RC_SIGN_IN);
+//    }
+//
+//    public void loginAsGuest(View view) {
+//        mAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+//            @Override
+//            public void onComplete(@NonNull Task<AuthResult> task) {
+//                if (task.isSuccessful()) {
+//                    Log.d(TAG, "Anonym user logged in successfully");
+//                    startShopping();
+//                } else {
+//                    Log.d(TAG, "Anonym user login fail");
+//                    Toast.makeText(MainActivity.this, "User login fail: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
+//                }
+//            }
+//        });
+//    }
 
-    private void firebaseAuthWithGoogle(String idToken) {
-        AuthCredential credential = GoogleAuthProvider.getCredential(idToken, null);
-        mAuth.signInWithCredential(credential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    Log.d(TAG, "User logged in successfully!");
-                    startShopping();
-                } else {
-                    Log.d(TAG, "Something went wrong...");
-                    Toast.makeText(MainActivity.this, "User login fail: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-
-    }
-
-    public void login(View view) {
-        String userName = userNameET.getText().toString();
-        String password = passwordET.getText().toString();
-
-//        Log.i(TAG, "login: " + userName + "passwd: " + password);
-
-        mAuth.signInWithEmailAndPassword(userName, password).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    Log.d(TAG, "User logged in successfully!");
-                    startShopping();
-                } else {
-                    Log.d(TAG, "Something went wrong...");
-                    Toast.makeText(MainActivity.this, "User login fail: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-    }
-
-    private void startShopping() {
-        Intent intent = new Intent(this, ShopListActivity.class);
-        startActivity(intent);
-    }
-
-    public void loginWithGoogle(View view) {
-        Intent signInIntent = mGoogleSignInClient.getSignInIntent();
-        startActivityForResult(signInIntent, RC_SIGN_IN);
-    }
-
-    public void loginAsGuest(View view) {
-        mAuth.signInAnonymously().addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    Log.d(TAG, "Anonym user logged in successfully");
-                    startShopping();
-                } else {
-                    Log.d(TAG, "Anonym user login fail");
-                    Toast.makeText(MainActivity.this, "User login fail: " + task.getException().getMessage(), Toast.LENGTH_LONG).show();
-                }
-            }
-        });
-    }
-
-    public void register(View view) {
-        Intent intent = new Intent(this, RegisterActivity.class);
-        intent.putExtra("SECRET_KEY", SECRET_KEY);
-        startActivity(intent);
-        // TODO
-    }
+//    public void register(View view) {
+//        Intent intent = new Intent(this, RegisterActivity.class);
+//        intent.putExtra("SECRET_KEY", SECRET_KEY);
+//        startActivity(intent);
+//        // TODO
+//    }
 
     @Override
     protected void onStart() {
@@ -174,10 +178,10 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
     @Override
     protected void onPause() {
         super.onPause();
-        SharedPreferences.Editor editor = preferences.edit();
-        editor.putString("userName", userNameET.getText().toString());
-        editor.putString("password", passwordET.getText().toString());
-        editor.apply();
+//        SharedPreferences.Editor editor = preferences.edit();
+//        editor.putString("userName", userNameET.getText().toString());
+//        editor.putString("password", passwordET.getText().toString());
+//        editor.apply();
 
         Log.i(TAG, "onPause");
     }
@@ -194,20 +198,49 @@ public class MainActivity extends AppCompatActivity implements LoaderManager.Loa
         Log.i(TAG, "onRestart");
     }
 
-    @NonNull
-    @Override
-    public Loader<String> onCreateLoader(int id, @Nullable Bundle args) {
-        return new RandomAsyncLoader(this);
+    public void goToRegist(View view) {
+        Intent intent = new Intent(this, RegisterActivity.class);
+        startActivity(intent);
     }
 
-    @Override
-    public void onLoadFinished(@NonNull Loader<String> loader, String data) {
-        Button button = findViewById(R.id.guestLoginButton);
-        button.setText(data);
+    public void goToLogin(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        startActivity(intent);
     }
 
-    @Override
-    public void onLoaderReset(@NonNull Loader<String> loader) {
-
+    public void goToFoodList(View view) {
+        Intent intent = new Intent(this, LoginActivity.class);
+        intent.putExtra("SECRET_KEY", SECRET_KEY);
+        startActivity(intent);
+        // TODO: putextra with the secret key, default 99, logged in 98,
+        //  if default: only list, else ability to add to the intake collection, or remove from it
     }
+
+    public void goToIntake(View view) {
+        Intent intent = new Intent(this, ShopListActivity.class);
+        if (SECRET_KEY == 98){
+            startActivity(intent);
+        }
+        else{
+            Toast.makeText(this, "You have to log in to access the Daily Intake page!", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+//    @NonNull
+//    @Override
+//    public Loader<String> onCreateLoader(int id, @Nullable Bundle args) {
+//        return new RandomAsyncLoader(this);
+//    }
+//
+//    @Override
+//    public void onLoadFinished(@NonNull Loader<String> loader, String data) {
+//        Button button = findViewById(R.id.guestLoginButton);
+//        button.setText(data);
+//    }
+//
+//    @Override
+//    public void onLoaderReset(@NonNull Loader<String> loader) {
+//
+//    }
 }
