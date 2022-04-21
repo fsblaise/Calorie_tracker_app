6bcc1bc1461854ac848bdb2356941bd3abd7e42b
fsblaise <balazsolah19@gmail.com>
2022-04-21 11:32:48 +0200

    Implemented the intake collection into the Intake Activity. Now you users can check the food they have consumed. Users can remove items from collection in the Intake Activity as well.

--- app/src/main/java/hu/fsblaise/kcal/IntakeActivity.java
+++ app/src/main/java/hu/fsblaise/kcal/IntakeActivity.java
@@ -1,14 +1,288 @@
 package hu.fsblaise.kcal;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.view.MenuItemCompat;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.app.job.JobInfo;
+import android.app.job.JobScheduler;
+import android.content.BroadcastReceiver;
+import android.content.ComponentName;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.content.SharedPreferences;
+import android.content.res.TypedArray;
 import android.os.Bundle;
+import android.os.SystemClock;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.Query;
+import com.google.firebase.firestore.QueryDocumentSnapshot;
+
+import java.util.ArrayList;
+import java.util.Objects;
+
+import static android.view.View.GONE;
+import static android.view.View.VISIBLE;
 
 public class IntakeActivity extends AppCompatActivity {
+    private static final String LOG_TAG = IntakeActivity.class.getName();
+    private static final String PREF_KEY = MainActivity.class.getPackage().toString();
+    private FirebaseUser user;
+
+    private FrameLayout redCircle;
+    private TextView countTextView;
+    private int cartItems = 0;
+    private int gridNumber = 1;
+    private int queryLimit = 1000;
+
+    // Member variables.
+    private RecyclerView mRecyclerView;
+    private ArrayList<FoodItem> mItems2Data;
+    private IntakeAdapter mAdapter;
+
+    private FirebaseFirestore mFirestore;
+    private CollectionReference mItems2;
+
+    private NotificationHandler mNotificationHandler;
+    private AlarmManager mAlarmManager;
+    private JobScheduler mJobScheduler;
+
+    private SharedPreferences preferences;
+
+    private boolean viewRow = true;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intake);
+
+        user = FirebaseAuth.getInstance().getCurrentUser();
+
+        //Commented out, because it will block the guest login
+
+//        if (user != null && !user.getEmail().equals("null")) {
+//            Log.d(LOG_TAG, "Authenticated user!" + user.getEmail());
+//        } else {
+//            Log.d(LOG_TAG, "Unauthenticated user!");
+//            finish();
+//        }
+
+/*        preferences = getSharedPreferences(PREF_KEY, MODE_PRIVATE);
+        if(preferences != null) {
+            cartItems = preferences.getInt("cartItems", 0);
+            gridNumber = preferences.getInt("gridNum", 1);
+        }*/
+
+        // recycle view
+        mRecyclerView = findViewById(R.id.recyclerView2);
+        // Set the Layout Manager.
+        mRecyclerView.setLayoutManager(new GridLayoutManager(
+                this, gridNumber));
+        // Initialize the ArrayList that will contain the data.
+        mItems2Data = new ArrayList<>();
+        // Initialize the adapter and set it to the RecyclerView.
+        mAdapter = new IntakeAdapter(this, mItems2Data);
+        mRecyclerView.setAdapter(mAdapter);
+
+        mFirestore = FirebaseFirestore.getInstance();
+        mItems2 = mFirestore.collection((user.getEmail() != null) ? "Intake: " + user.getEmail() : "Intake: Default");
+        // Get the data.
+        queryData2();
+
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(Intent.ACTION_POWER_CONNECTED);
+        filter.addAction(Intent.ACTION_POWER_DISCONNECTED);
+        this.registerReceiver(powerReceiver, filter);
+
+        mNotificationHandler = new NotificationHandler(this);
+        mAlarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+        mJobScheduler = (JobScheduler) getSystemService(JOB_SCHEDULER_SERVICE);
+
+        //setAlarmManager();
+        setJobScheduler();
+
+    }
+
+    BroadcastReceiver powerReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+
+            if (action == null)
+                return;
+
+            switch (action) {
+                case Intent.ACTION_POWER_CONNECTED:
+                    queryLimit = 1000;
+                    break;
+                case Intent.ACTION_POWER_DISCONNECTED:
+                    queryLimit = 5;
+                    break;
+            }
+
+            queryData2();
+        }
+    };
+
+    private void queryData2(){
+        mItems2Data.clear();
+        mItems2.orderBy("cartedCount", Query.Direction.DESCENDING).limit(queryLimit).get()
+                .addOnSuccessListener(queryDocumentSnapshots -> {
+                    for (QueryDocumentSnapshot document : queryDocumentSnapshots) {
+                        FoodItem item = document.toObject(FoodItem.class);
+                        item.setId(document.getId());
+                        mItems2Data.add(item);
+                    }
+
+//                    if (mItems2Data.size() == 0) {
+//                        return;
+//                    }
+
+                    // Notify the adapter of the change.
+                    mAdapter.notifyDataSetChanged();
+                });
+    }
+
+    public void removeItem(FoodItem item) {
+        for (int i = 0; i < mItems2Data.size(); i++) {
+            if (mItems2Data.get(i).getName().equals(item.getName())){
+                if (mItems2Data.get(i).getCartedCount() > 0){
+                    String id = mItems2Data.get(i)._getId();
+                    mItems2.document(id).update("cartedCount", mItems2Data.get(i).getCartedCount() - 1).addOnFailureListener(failure -> {
+                        Toast.makeText(this, "Item " + id + " cannot be changed.", Toast.LENGTH_SHORT).show();
+                    });
+                } else {
+                    String id = mItems2Data.get(i)._getId();
+                    DocumentReference ref = mItems2.document(mItems2Data.get(i)._getId());
+                    ref.delete().addOnSuccessListener(succes -> {
+                        Log.d(LOG_TAG, "Item is successfully deleted: " + id);
+                    })
+                            .addOnFailureListener(failure -> {
+                                Toast.makeText(this, "Item  " + id + " cannot be deleted.", Toast.LENGTH_SHORT).show();
+                            });
+                }
+            }
+        }
+        queryData2();
+        mNotificationHandler.cancel();
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.intake_menu, menu);
+        MenuItem menuItem = menu.findItem(R.id.search_bar);
+        SearchView searchView = (SearchView) MenuItemCompat.getActionView(menuItem);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return false;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                Log.d(LOG_TAG, s);
+                mAdapter.getFilter().filter(s);
+                return false;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.log_out_button:
+                Log.d(LOG_TAG, "Logout clicked!");
+                FirebaseAuth.getInstance().signOut();
+                finish();
+                return true;
+            case R.id.settings_button:
+                Log.d(LOG_TAG, "Setting clicked!");
+//                FirebaseAuth.getInstance().signOut();
+//                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+
+    private void changeSpanCount(MenuItem item, int drawableId, int spanCount) {
+        viewRow = !viewRow;
+        item.setIcon(drawableId);
+        GridLayoutManager layoutManager = (GridLayoutManager) mRecyclerView.getLayoutManager();
+        layoutManager.setSpanCount(spanCount);
+    }
+
+//    @Override
+//    public boolean onPrepareOptionsMenu(Menu menu) {
+//        final MenuItem alertMenuItem = menu.findItem(R.id.cart);
+//        FrameLayout rootView = (FrameLayout) alertMenuItem.getActionView();
+//
+//        redCircle = (FrameLayout) rootView.findViewById(R.id.view_alert_red_circle);
+//        countTextView = (TextView) rootView.findViewById(R.id.view_alert_count_textview);
+//
+//        rootView.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                onOptionsItemSelected(alertMenuItem);
+//            }
+//        });
+//        return super.onPrepareOptionsMenu(menu);
+//    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        unregisterReceiver(powerReceiver);
+    }
+
+    private void setAlarmManager() {
+        long repeatInterval = 1 * 60 * 1000;//AlarmManager.INTERVAL_FIFTEEN_MINUTES;
+        long triggerTime = SystemClock.elapsedRealtime() + repeatInterval;
+
+        Intent intent = new Intent(this, AlarmReceiver.class);
+        PendingIntent pendingIntent = PendingIntent.getBroadcast(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+
+        mAlarmManager.setInexactRepeating(
+                AlarmManager.ELAPSED_REALTIME_WAKEUP,
+                triggerTime,
+                repeatInterval,
+                pendingIntent
+        );
+
+        // mAlarmManager.cancel(pendingIntent);
+    }
+
+    private void setJobScheduler() {
+        int networkType = JobInfo.NETWORK_TYPE_UNMETERED;
+        int hardDeadLine = 5000;
+
+        ComponentName name = new ComponentName(getPackageName(), NotificationJobService.class.getName());
+        JobInfo.Builder builder = new JobInfo.Builder(0, name)
+                .setRequiredNetworkType(networkType)
+                .setRequiresCharging(true)
+                .setOverrideDeadline(hardDeadLine);
+
+        mJobScheduler.schedule(builder.build());
+//        mJobScheduler.cancel(0);
     }
 }
---
+++ app/src/main/java/hu/fsblaise/kcal/IntakeAdapter.java
@@ -0,0 +1,124 @@
+package hu.fsblaise.kcal;
+
+import android.content.Context;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.Filter;
+import android.widget.Filterable;
+import android.widget.TextView;
+import androidx.recyclerview.widget.RecyclerView;
+import java.util.ArrayList;
+
+public class IntakeAdapter extends RecyclerView.Adapter<IntakeAdapter.ViewHolder> implements Filterable {
+    private static final String LOG_TAG = MainActivity.class.getName();
+    // Member variables.
+    private ArrayList<FoodItem> mFoodItemData = new ArrayList<>();
+    private ArrayList<FoodItem> mFoodItemDataAll = new ArrayList<>();
+    private Context mContext;
+    private int lastPosition = -1;
+
+    IntakeAdapter(Context context, ArrayList<FoodItem> itemsData) {
+        this.mFoodItemData = itemsData;
+        this.mFoodItemDataAll = itemsData;
+        this.mContext = context;
+    }
+
+    @Override
+    public IntakeAdapter.ViewHolder onCreateViewHolder(
+            ViewGroup parent, int viewType) {
+        return new ViewHolder(LayoutInflater.from(mContext)
+                .inflate(R.layout.list_intake, parent, false));
+    }
+
+    @Override
+    public void onBindViewHolder(IntakeAdapter.ViewHolder holder, int position) {
+        // Get current sport.
+        FoodItem currentItem = mFoodItemData.get(position);
+
+        // Populate the textviews with data.
+        holder.bindTo(currentItem);
+
+
+        if (holder.getAdapterPosition() > lastPosition) {
+            Animation animation = AnimationUtils.loadAnimation(mContext, R.anim.slide_in_row);
+            holder.itemView.startAnimation(animation);
+            lastPosition = holder.getAdapterPosition();
+        }
+    }
+
+    @Override
+    public int getItemCount() {
+        return mFoodItemData.size();
+    }
+
+
+    /**
+     * RecycleView filter
+     **/
+    @Override
+    public Filter getFilter() {
+        return ShoppingFilter;
+    }
+
+    private Filter ShoppingFilter = new Filter() {
+        @Override
+        protected FilterResults performFiltering(CharSequence charSequence) {
+            ArrayList<FoodItem> filteredList = new ArrayList<>();
+            FilterResults results = new FilterResults();
+
+            if (charSequence == null || charSequence.length() == 0) {
+                results.count = mFoodItemDataAll.size();
+                results.values = mFoodItemDataAll;
+            } else {
+                String filterPattern = charSequence.toString().toLowerCase().trim();
+                for (FoodItem item : mFoodItemDataAll) {
+                    if (item.getName().toLowerCase().contains(filterPattern)) {
+                        filteredList.add(item);
+                    }
+                }
+
+                results.count = filteredList.size();
+                results.values = filteredList;
+            }
+
+            return results;
+        }
+
+        @Override
+        protected void publishResults(CharSequence charSequence, FilterResults filterResults) {
+            mFoodItemData = (ArrayList) filterResults.values;
+            notifyDataSetChanged();
+        }
+    };
+
+    class ViewHolder extends RecyclerView.ViewHolder {
+
+        // Member Variables for the TextViews
+        private TextView mTitleText;
+        private TextView mPriceText;
+        private TextView mCountText;
+
+        ViewHolder(View itemView) {
+            super(itemView);
+
+            // Initialize the views.
+            mTitleText = itemView.findViewById(R.id.itemTitle);
+            mPriceText = itemView.findViewById(R.id.price);
+            mCountText = itemView.findViewById(R.id.count);
+        }
+
+        void bindTo(FoodItem currentItem) {
+            mTitleText.setText(currentItem.getName());
+            mPriceText.setText(currentItem.getCalories());
+            String count = "" + (currentItem.getCartedCount() + 1);
+            mCountText.setText(count);
+
+            itemView.findViewById(R.id.remove).setOnClickListener(view -> ((IntakeActivity) mContext).removeItem(currentItem));
+        }
+    }
+}
+
